2dc88eb0ec73674da3e3b9bacbd693d7104ab60b
Adrian Urresta <152809078+Mclxv1nx@users.noreply.github.com>
2024-05-23 11:43:39 -0500

    Metodo
    metodo de newton

---
+++ NewtonMetodo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newton_Raphson
+{
+    internal class NewtonMetodo
+    {
+        public static (double root, int iterations) FindRoot(Func<double, double> f, Func<double, double> df, double x0, double tol = 1e-10, int maxIter = 100)
+        {
+            double x = x0;
+            for (int i = 0; i < maxIter; i++)
+            {
+                double xNew = x - f(x) / df(x);
+                if (Math.Abs(xNew - x) < tol)
+                {
+                    return (xNew, i + 1);
+                }
+                x = xNew;
+            }
+            return (x, maxIter);
+        }
+    }
+}
