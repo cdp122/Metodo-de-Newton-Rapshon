47fa9bcc2c87e6d85ba0632d9c17afb793a7cc89
Adrian Urresta <152809078+Mclxv1nx@users.noreply.github.com>
2024-05-23 20:54:24 -0500

    quitar metodo inservible
    chao

--- NewtonMetodo.cs
+++ NewtonMetodo.cs
@@ -2,23 +2,7 @@
 
 namespace Newton_Raphson
 {
-    internal class NewtonMetodo
-    {
-        public static (double root, int iterations) FindRoot(Func<double, double> f, Func<double, double> df, double x0, double tol = 1e-10, int maxIter = 100)
-        {
-            double x = x0;
-            for (int i = 0; i < maxIter; i++)
-            {
-                double xNew = x - f(x) / df(x);
-                if (Math.Abs(xNew - x) < tol)
-                {
-                    return (xNew, i + 1);
-                }
-                x = xNew;
-            }
-            return (x, maxIter);
-        }
-    }
+    
 
     internal class Derivadas
     {
